300312e02f157a0027ced32e317190501d670058
Ernest <ernestduodu@hotmail.co.uk>
2021-09-08 08:35:34 +0100

    Formatting code

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,24 +3,25 @@ import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
-
 const HeaderCartButton = ({ onClick }) => {
-  const [bump, setBump] = useState(false)
+  const [bump, setBump] = useState(false);
   const context = useContext(CartContext);
-  const numberOfCartItems = context.items.reduce((current, item) => current + item.amount, 0)
-  
+  const numberOfCartItems = context.items.reduce(
+    (current, item) => current + item.amount,
+    0
+  );
+
   const btnClasses = `${styles.button} ${bump && styles.bump}`;
 
   useEffect(() => {
-      if(context.items.length === 0) return;
+    if (context.items.length === 0) return;
 
-    setBump(true)
+    setBump(true);
     const timer = setTimeout(() => {
-         setBump(false)
-     }, 300);
-     return () => clearTimeout(timer)
-  }, [context.items])
- 
+      setBump(false);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [context.items]);
 
   return (
     <button className={btnClasses} onClick={onClick}>
